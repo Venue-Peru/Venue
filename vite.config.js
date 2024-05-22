6629aca9f6be6b4ee13993d081ecb817b8e529f0
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-05-21 20:26:48 -0500

    chore: edited metadata in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
