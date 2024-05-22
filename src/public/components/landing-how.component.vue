42f8d91b53bc54ac03b372ac6c7d2e1d2eec9950
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-05-21 19:50:44 -0500

    feat: implemented footer and how sections from landing page

--- src/public/components/landing-how.component.vue
+++ src/public/components/landing-how.component.vue
@@ -5,9 +5,15 @@ export default {
 </script>
 
 <template>
-  <div class="bg-gray-900 h-30rem border-round">
+  <div class="bg-gray-900 border-round text-white">
     <div class="col-12 justify-content-center align-content-center text-center">
-      <pv-button label="Explorar" severity="secondary"/>
+      <h1 class="mt-3">¿Cómo Funciona?</h1>
+    </div>
+    <div class="col-12 justify-content-center align-content-center text-center">
+      <img height="300" src="../../assets/wireframe-box.jpg">
+    </div>
+    <div class="col-12 justify-content-center align-content-center text-center">
+      <pv-button label="Explorar" severity="secondary" class="mb-3"/>
     </div>
   </div>
 </template>
--- src/public/pages/landing-page.component.vue
+++ src/public/pages/landing-page.component.vue
@@ -1,5 +1,5 @@
 <script>
-import LandingToolbar from "../components/landing-toolbar.component.vue";
+import LandingHeader from "../components/landing-header.component.vue";
 import LandingHero from "../components/landing-hero.component.vue";
 import LandingHow from "../components/landing-how.component.vue";
 import LandingFaqs from "../components/landing-faqs.component.vue";
@@ -8,18 +8,23 @@ import LandingFooter from "../components/landing-footer.component.vue";
 
 export default {
   name: "landing-page",
-  components: {LandingFooter, LandingForm, LandingHow, LandingToolbar, LandingHero, LandingFaqs}
+  components: {LandingFooter, LandingForm, LandingHow, LandingHeader, LandingHero, LandingFaqs}
 }
 </script>
 
 <template>
-  <landing-toolbar class="m-3"/>
-  <landing-hero class="m-3"/>
-  <landing-how class="m-3"/>
-  <landing-faqs class="m-3"/>
-  <landing-form class="m-3"/>
-  <landing-footer class="m-3"/>
+  <div class="landing-page pt-1 pb-1">
+    <landing-header class="mt-3 mr-3 ml-3"/>
+    <landing-hero class="m-3"/>
+    <landing-how class="m-3"/>
+    <landing-faqs class="m-3"/>
+    <landing-form class="m-3"/>
+    <landing-footer class="mb-3 mr-3 ml-3"/>
+  </div>
 </template>
 
 <style>
+.landing-page {
+  background-image: url("../../assets/Flashing-Lights-thumbnail-seamless.jpg");
+}
 </style>
